c705f0be431d82ccd6e8535b0bce252d9f101e10
Lyduke56 <rosalclydejustine@gmail.com>
2026-02-17 22:07:16 +0800

    [Day 1] - Introduction

---
+++ day1.py
@@ -0,0 +1,22 @@
+# Introduction
+# Day 1 of 30
+print("Day 1 of Python Practice")
+
+# Basic Mathematical Operations
+print(2 + 3)    # addition, = 5
+print(3 - 1)    # subtraction, = 2
+print(2 * 3)    # multiplication, = 6
+print(10 / 2)   # division, = 5
+print(15 ** 2)  # exponential, = 225
+print(19 % 6)   # modulus, = 1
+print(5 // 2)   # floor division, 5 / 2 = 2.5 = 2, lowest whole number lwk trippy
+
+# Checking data types
+print(type(10))                         # int data type, decimal numbers
+print(type(3.14))                       # float data type, floating point numbers
+print(type(1 + 3j))                     # complex data type, imaginary number/equation
+print(type('Lyduke Test Test'))         # string data type, characters
+print(type({'name': 'Lyduke'}))         # dictionary data type, key value pairs as in 'key' : 'value'
+print(type([1, 2, 3, 6, 7]))            # list data type, ordered, changeable, duplicates
+print(type({2, 3 ,6}))                  # set data type, unordered, changeable, no duplicates
+print(type((1, 1, 3, 5, 2)))            # tuple data type, ordered, unchangeable, duplicates
